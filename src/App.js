60a16c90cf61a0e46f9514e9a35cd0825b7375a4
Surya Teja Tangella <suryatejatangella234@gmail.com>
2026-04-09 08:23:15 +0530

    abort controller added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedAmount, setDebouncedAmount] = useState(amount);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedAmount(amount);
+    }, 500);
+    // 500ms delay
+
+    return () => clearTimeout(timer);
+  }, [amount]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function convert() {
+      try {
+        setIsLoading(true);
+
+        if (!debouncedAmount || debouncedAmount <= 0) {
+          setConverted("");
+          setError("Please enter a valid amount");
+          return;
+        }
+
+        const res = await fetch(
+          `https://api.frankfurter.dev/v1/latest?from=${fromCur}&to=${toCur}&amount=${debouncedAmount}`,
+          {
+            signal: controller.signal,
+          },
+        );
+
+        // if (!res.okay) throw new Error("Failed to fetch");
+
+        const data = await res.json();
+        setConverted(data.rates[toCur]);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError("Conversion Failed");
+          setConverted("");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (fromCur === toCur) {
+      setConverted(debouncedAmount);
+      return;
+    }
+
+    convert();
+
+    return () => {
+      controller.abort(); //cancel previous requeste
+    };
+  }, [debouncedAmount, fromCur, toCur]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Currency Converter</h1>
+      <input
+        value={amount}
+        type="text"
+        placeholder="Enter Amount to convert"
+        onChange={(e) => {
+          const value = Number(e.target.value);
+          setAmount(isNaN(value) ? "" : value);
+          setError("");
+        }}
+        // disabled={isLoading}
+      />
+      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+        <option value="USD">USD - $</option>
+        <option value="EUR">EUR - €</option>
+        <option value="CAD">CAD - $</option>
+        <option value="INR">INR - ₨</option>
+      </select>
+      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+        <option value="USD">USD - $</option>
+        <option value="EUR">EUR - €</option>
+        <option value="CAD">CAD - $</option>
+        <option value="INR">INR - ₨</option>
+      </select>
+      <p>
+        Output :
+        <span style={{ fontWeight: 600 }}>
+          {" "}
+          {isLoading ? "Converting..." : error ? error : converted}{" "}
+        </span>{" "}
+        {toCur}
+      </p>
     </div>
   );
 }
-
-export default App;
